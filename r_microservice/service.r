e92a6b7dd56c957a10412e35555ccc43b8077828
Jan Kjerbye <jmk@randers.dk>
2023-06-21 14:52:12 +0200

    trying a new approach

--- r_microservice/service.r
+++ r_microservice/service.r
@@ -1,53 +1,20 @@
-# Load required libraries
-library(httr)
+library(plumber)
 library(jsonlite)
-library(Rook)
+library(tidyverse)
 
-# Define the transform function
-transform_function <- function(data) {
-  print("inside transform_function")
-  # Calculate the mean of a numeric column in the data frame
-  result <- mean(data$numeric_column)
-  print(paste("result:", result))
-  return(result)
+#* @post /transform
+function(req){
+  # Get the JSON data from the request
+  data <- fromJSON(content(req$postBody))
+  
+  # Add your custom data transformations here
+  df <- as_tibble(data)
+  transformed_data <- data
+  
+  # Return the transformed data as a JSON string
+  return(toJSON(transformed_data))
 }
 
-# Create an HTTP server
-server <- Rook::Server$new()
-
-# Define the /transform endpoint
-server$add(app = Rook::URLMap$new(), "/", function(env) {
-  print("inside /transform endpoint")
-  # Check if this is a POST request
-  if (env$REQUEST_METHOD == "POST") {
-    print("POST request")
-    # Read the request body
-    req_body <- rawToChar(env$rook.input$read())
-    
-    # Parse the JSON data from the request body
-    data <- fromJSON(req_body)
-    
-    # Convert the data to a data frame
-    df <- as.data.frame(data)
-    
-    # Run the data through the transform function
-    print("before transform_function")
-    result <- transform_function(df)
-    print("after transform_function")
-    
-    # Return the result as JSON
-    res <- Rook::Response$new()
-    res$write(toJSON(result))
-    res$finish()
-  } else {
-    print("non-POST request")
-    # Return an error response for non-POST requests
-    res <- Rook::Response$new()
-    res$status <- 400
-    res$write("Bad Request")
-    res$finish()
-  }
-})
-
-# Start the server on port 8888
-server$start(port = 8888)
+# Run the Plumber app on host 0.0.0.0 (accessible from any IP address)
+pr <- plumb()
+pr$run(host='0.0.0.0')
