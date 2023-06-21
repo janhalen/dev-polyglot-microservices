d60ebf61eb5c09aa17846a52d4b320c3490fd0a1
Jan Kjerbye <jmk@randers.dk>
2023-06-21 14:45:10 +0200

    still hunting bugs

--- r_microservice/service.r
+++ r_microservice/service.r
@@ -1,11 +1,14 @@
 # Load required libraries
 library(httr)
 library(jsonlite)
+library(Rook)
 
 # Define the transform function
 transform_function <- function(data) {
+  print("inside transform_function")
   # Calculate the mean of a numeric column in the data frame
   result <- mean(data$numeric_column)
+  print(paste("result:", result))
   return(result)
 }
 
@@ -14,8 +17,10 @@ server <- Rook::Server$new()
 
 # Define the /transform endpoint
 server$add(app = Rook::URLMap$new(), "/", function(env) {
+  print("inside /transform endpoint")
   # Check if this is a POST request
   if (env$REQUEST_METHOD == "POST") {
+    print("POST request")
     # Read the request body
     req_body <- rawToChar(env$rook.input$read())
     
@@ -26,13 +31,16 @@ server$add(app = Rook::URLMap$new(), "/", function(env) {
     df <- as.data.frame(data)
     
     # Run the data through the transform function
+    print("before transform_function")
     result <- transform_function(df)
+    print("after transform_function")
     
     # Return the result as JSON
     res <- Rook::Response$new()
     res$write(toJSON(result))
     res$finish()
   } else {
+    print("non-POST request")
     # Return an error response for non-POST requests
     res <- Rook::Response$new()
     res$status <- 400
