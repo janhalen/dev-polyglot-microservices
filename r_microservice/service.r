ae96396a1d89290ec26dc3cf9a8c93588372e11b
Jan Kjerbye <jmk@randers.dk>
2023-06-21 20:57:31 +0200

    trying fiery lib

--- r_microservice/service.r
+++ r_microservice/service.r
@@ -1,16 +1,20 @@
-library(jug)
+library(fiery)
 library(jsonlite)
+library(tibble)
 
-jug() %>%
-  post("/transform", function(req, res, err){
-    # Get the JSON data from the request
-    data <- fromJSON(content(req$body))
-    
-    # Add your custom data transformations here
-    df <- as_tibble(data)
-    transformed_data <- data
-    
-    # Return the transformed data as a JSON string
-    res$send_json(transformed_data)
-  }) %>%
-  serve_it()
+app <- Fire$new()
+
+app$add_route("POST", "/transform", function(req, res) {
+  # Get the JSON data from the request
+  data <- fromJSON(content(req$body))
+  
+  # Add your custom data transformations here
+  df <- as_tibble(data)
+  transformed_data <- data
+  
+  # Return the transformed data as a JSON string
+  res$set_body(toJSON(transformed_data))
+  res$set_header("Content-Type", "application/json")
+})
+
+app$start(port = 8888)
