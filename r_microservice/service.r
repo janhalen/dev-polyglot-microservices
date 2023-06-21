4b09f3ebce71b93ca87eab04a892ee37ac4af21c
Jan Kjerbye <jmk@randers.dk>
2023-06-21 21:35:02 +0200

    more debugging

--- r_microservice/service.r
+++ r_microservice/service.r
@@ -1,24 +1,22 @@
+print("before fiery")
 library(fiery)
+print("after fiery")
+
+print("before jsonlite")
 library(jsonlite)
+print("after jsonlite")
+
+print("before tibble")
 library(tibble)
+print("after tibble")
 
 app <- Fire$new()
 
 app$add_route("POST", "/transform", function(req, res) {
-  print("before fromJSON")
   data <- fromJSON(content(req$body))
-  print("after fromJSON")
-  
-  print("before as_tibble")
   df <- as_tibble(data)
-  print("after as_tibble")
-  
   transformed_data <- data
-  
-  print("before toJSON")
   res$set_body(toJSON(transformed_data))
-  print("after toJSON")
-  
   res$set_header("Content-Type", "application/json")
 })
 
